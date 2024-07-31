7edbf3531da6dd880ce5e8e0b1a3a51609ac8f63
heeje <heeje1996@gmail.com>
2024-08-01 08:58:14 +0900

    Update: Cors & SQL2 (#2)

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 // server.js
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+// 기본적인 CORS 설정 (모든 도메인 허용)
+app.use(cors());
 
 // View engine setup, if needed
 // app.set('view engine', 'ejs');
@@ -12,7 +17,7 @@ const accountRouter = require('./router/accountRouter');
 app.use(express.json()); // to parse JSON bodies
 
 const port = 8080;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Listening on ' + port);
 });
 
